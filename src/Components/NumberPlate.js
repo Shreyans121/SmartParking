dfcd5c2d7ddf0db58ae642c3af0230f239b8aaa0
Shreyans Jain <jainshreyans121@gmail.com>
2022-03-20 00:26:16 +0530

    first commit

---
+++ src/Components/NumberPlate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NumberPlate = () => {
+  return (
+      <div style = {{margin:'2rem'}}>
+        <h3>List of Cars Parked</h3>
+        <div>
+          <ol>
+            <li>DL69</li>
+            <li>DL69</li>
+            <li>DL69</li>
+          </ol>
+        </div>
+      </div>
+  );
+};
+
+export default NumberPlate;
---
+++ src/Components/ParkingSpots.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ParkingSpots = () => {
+  return (
+      <div style = {{margin:'2rem'}}>
+        <h3>List of Cars Parked</h3>
+        <div>
+          <ol>
+            <li>A1 <span>Empty</span></li>
+            <li>A2 <span>Empty</span></li>
+            <li>A3 <span>Empty</span></li>
+          </ol>
+        </div>
+      </div>
+  );
+};
+
+export default ParkingSpots;
